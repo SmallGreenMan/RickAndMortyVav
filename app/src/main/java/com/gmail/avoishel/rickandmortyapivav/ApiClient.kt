20ed8fc1d54b7f31e22414a7ba4db7ec8b5dda04
Alexandr Voishel <avoishel@gmail.com>
2022-05-12 14:11:34 +0300

    add: reaction on falied network request

--- app/src/main/java/com/gmail/avoishel/rickandmortyapivav/ApiClient.kt
+++ app/src/main/java/com/gmail/avoishel/rickandmortyapivav/ApiClient.kt
@@ -6,7 +6,16 @@ import retrofit2.Response
 class ApiClient(
     private val rickAndMortyService: RickAndMortyService
 ) {
-    suspend fun getCharacterById(characterId: Int): Response<GetCharacterByIdResponse> {
-        return rickAndMortyService.getCharacterById(characterId)
+    suspend fun getCharacterById(characterId: Int): SimpleResponse<GetCharacterByIdResponse> {
+        //return rickAndMortyService.getCharacterById(characterId)
+        return safeApiCall { rickAndMortyService.getCharacterById(characterId) }
+    }
+
+    private inline fun <T> safeApiCall(apiCall: () -> Response<T>): SimpleResponse<T> {
+        return try {
+            SimpleResponse.success(apiCall.invoke())
+        } catch (e: Exception) {
+            SimpleResponse.failure(e)
+        }
     }
 }
